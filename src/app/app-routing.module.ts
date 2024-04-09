fd9fa5580e3a413c00ca2f641c7a71c0b6d6b0a2
leonelVc19 <vazquez.juan.1fm@gmail.com>
2024-04-08 19:10:41 -0600

    Frontend de Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    //guards
+    loadChildren: () => import('./auth/auth.module').then( moduleA => moduleA.AuthModule ),
+  },
+  {
+    path: 'dashboard',
+    //guards
+    loadChildren: () => import('./dashboard/dashboard.module').then( moduleD => moduleD.DashboardModule ),
+  },
+  { path: '**', redirectTo: 'auth' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
